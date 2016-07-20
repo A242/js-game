6503750a760148982783cb08e1a8b9c656167e04
A242 <yorge360@gmail.com>
2016-07-20 12:07:29 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 
-var player = {
-  x : 0,
-  y : 100,
-  velX : 0,
-  velY : 0,
-  sprite : document.getElementById("player"),
-  update: function(){
-    this.x += this.velX;
-    this.y += this.velY;
-  },
-  render: function(){
-    ctx.drawImage(this.sprite, this.x, this.y);
-  }
-};
-
 var background = document.getElementById("background");
 c.width = window.innerWidth;
 c.height = window.innerHeight;
@@ -27,18 +12,3 @@ function animate(){
 }
 
 animate();
-
-$(document).keydown(function(e){
-  if (e.keyCode == 37) player.velX = -5;
-  if (e.keyCode == 38) player.velY = -5;
-  if (e.keyCode == 39) player.velX = 5;
-  if (e.keyCode == 40) player.velY = 5;
-  if (e.keyCode == 32) document.getElementById("audio").play();
-});
-
-$(document).keyup(function(e){
-  if (e.keyCode == 37) player.velX = 0;
-  if (e.keyCode == 38) player.velY = 0;
-  if (e.keyCode == 39) player.velX = 0;
-  if (e.keyCode == 40) player.velY = 0;
-});
