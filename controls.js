965baade34ac5a5dd8a9ad49e2fa70504cc671ec
A242 <yorge360@gmail.com>
2016-07-22 10:03:31 -0400

    Add files via upload

--- controls.js
+++ controls.js
@@ -17,13 +17,18 @@ $(document).keyup(function(e){
   if (e.keyCode == 39) player.velX = 0;
   if (e.keyCode == 40) player.velY = 0;
 });
-$(document).on("touchstart", function(e){
+$(document).on("touchmove", function(e){
   //where the touch happens
   // e.touches[0].clientX, e.touches[0].clientY
+  e.preventDefault();
   var touchX = e.touches[0].clientX;
   var touchY = e.touches[0].clientY;
   if (touchX < player.x) player.velX = -5;
   if (touchX > player.x) player.velX = 5;
   if (touchY < player.y) player.velY = -5;
   if (touchY > player.y) player.velY = 5;
+});
+$(document).on("touchend", function (e){
+player.velX = 0;
+player.velY = 0;
 });
